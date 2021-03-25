1fba36d5de5825143083b17abcf621799aa7e9c1
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-03-25 21:43:14 +0900

    Update UIManager.cs

--- Breakout/Scripts/UIManager.cs
+++ Breakout/Scripts/UIManager.cs
@@ -7,41 +7,51 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject Panel;
     [SerializeField] GameObject GameOverText;
     [SerializeField] GameObject GameClearText;
-    public BlocksManager blocksmanager;
-    public EndWallScript endwallscript;
+    BlocksManager blocksmanager;
+    EndWallScript endwallscript;
     private bool gameclear;
     private bool gameover;
 
     void Start()
     {
-        gameclear = blocksmanager.deleteBlock;
-        gameover = endwallscript.hitBall;
         Panel = GameObject.Find("Panel");
         GameOverText = GameObject.Find("GameOverText");
         GameClearText = GameObject.Find("GameClearText");
 
-        Panel.SetActive(false);
-        GameOverText.SetActive(false);
-        GameClearText.SetActive(false);
+        blocksmanager = GameObject.Find("Blocks").GetComponent<BlocksManager>();
+        endwallscript = GameObject.Find("GameEndWall").GetComponent<EndWallScript>();
+
+        ResetUI();
     }
 
     void Update()
     {
+        gameclear = blocksmanager.deleteBlock;
+        gameover = endwallscript.hitBall;
+
         if (gameclear)
         {
-            RetryGame();
+            Return2Home();
         }
 
         if (gameover)
         {
-            Return2Home();
+            RetryGame();
         }
+    }
+
+    public void ResetUI()
+    {
+        Panel.SetActive(false);
+        GameOverText.SetActive(false);
+        GameClearText.SetActive(false);
     }
 
     public void RetryGame()
     {
         Panel.SetActive(true);
         GameOverText.SetActive(true);
+        gameclear = false;
 
     }
 
@@ -49,5 +59,6 @@ public class UIManager : MonoBehaviour
     {
         Panel.SetActive(true);
         GameClearText.SetActive(true);
+        gameover = false;
     }
 }
