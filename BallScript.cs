d148949e3a137e6eab19b6b9561a5cfa34020511
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-03-25 21:38:36 +0900

    Update BallScript.cs

--- BallScript.cs
+++ BallScript.cs
@@ -6,10 +6,33 @@ public class BallScript : MonoBehaviour
 {
     public float speed = 5f;
     Rigidbody myRigidbody;
+    public AudioClip bound_sound;
+    public AudioClip bar_sound;
+    public AudioClip delete_sound;
+    AudioSource audioSource;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
         myRigidbody.velocity = new Vector3(speed, 0f, speed);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            audioSource.PlayOneShot(bound_sound);
+        }
+
+        if(collision.gameObject.tag == "Bar")
+        {
+            audioSource.PlayOneShot(bar_sound);
+        }
+
+        if(collision.gameObject.tag == "Block")
+        {
+            audioSource.PlayOneShot(delete_sound);
+        }
     }
 }
