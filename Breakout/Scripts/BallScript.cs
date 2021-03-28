b0050e324af496e42aee0c30ebdd3ebc6195303e
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-03-28 23:48:47 +0900

    Update BallScript.cs

--- Breakout/Scripts/BallScript.cs
+++ Breakout/Scripts/BallScript.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
     public float speed = 5f;
+    public float minSpd = 5f;
+    public float maxSpd = 10f;
+    
     Rigidbody myRigidbody;
+    Transform myTransform;
+    
     public AudioClip bound_sound;
     public AudioClip bar_sound;
     public AudioClip delete_sound;
@@ -15,11 +20,28 @@ public class BallScript : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody>();
         myRigidbody.velocity = new Vector3(speed, 0f, speed);
+        myTransform = transform;
         audioSource = GetComponent<AudioSource>();
+    }
+    
+    void Update()
+    {
+        Vector3 velocity = myRigidbody.velocity;
+        float clampedSpd = Mathf.Clamp(velocity.magnitude, minSpd, maxSpd);
+        myRigdbody.velocity = velocity.normalized * clampedSpd;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.CompareTag("Bar"))
+        {
+            Vector3 barPos = collision.transform.position;
+            Vector3 ballPos = myTransform.position;
+            Vector3 direction = (ballPos - barPos).normalized;
+            
+            float speed = myRigidbody.velocity.magnitude;
+            myRigidbody.velocity = direction * speed;
+        }
         if(collision.gameObject.tag == "Wall")
         {
             audioSource.PlayOneShot(bound_sound);
