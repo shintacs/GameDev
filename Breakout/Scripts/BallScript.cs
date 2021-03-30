a6e3d634eda706e8121259fa2691be14b262c4d5
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-03-30 23:10:55 +0900

    Update BallScript.cs

--- Breakout/Scripts/BallScript.cs
+++ Breakout/Scripts/BallScript.cs
@@ -7,10 +7,12 @@ public class BallScript : MonoBehaviour
     public float speed = 5f;
     public float minSpd = 5f;
     public float maxSpd = 10f;
-    
+    public bool start;
+
     Rigidbody myRigidbody;
     Transform myTransform;
-    
+    BarControl barcontrol;
+
     public AudioClip bound_sound;
     public AudioClip bar_sound;
     public AudioClip delete_sound;
@@ -18,30 +20,43 @@ public class BallScript : MonoBehaviour
 
     void Start()
     {
+        start = false;
+
+        //barcontrol = GameObject.Find("Bar").GetComponent<BarControl>();
+
         myRigidbody = GetComponent<Rigidbody>();
-        myRigidbody.velocity = new Vector3(speed, 0f, speed);
-        myTransform = transform;
+        myTransform = transform;         //初期位置はバーと同じ位置
         audioSource = GetComponent<AudioSource>();
     }
-    
+
     void Update()
     {
-        Vector3 velocity = myRigidbody.velocity;
-        float clampedSpd = Mathf.Clamp(velocity.magnitude, minSpd, maxSpd);
-        myRigdbody.velocity = velocity.normalized * clampedSpd;
+        if (Input.GetMouseButtonDown(0) && !start)
+        {
+            myRigidbody.velocity = new Vector3(speed, 0f, speed);   //ボールの発射
+            start = true;
+
+        }
+        else //初期位置以外の場合、バーを自由に動かせるようにする
+        {
+            Vector3 velocity = myRigidbody.velocity;
+            float clampedSpd = Mathf.Clamp(velocity.magnitude, minSpd, maxSpd);
+            myRigidbody.velocity = velocity.normalized * clampedSpd;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Bar"))
+        if (collision.gameObject.CompareTag("Bar"))
         {
-            Vector3 barPos = collision.transform.position;
-            Vector3 ballPos = myTransform.position;
-            Vector3 direction = (ballPos - barPos).normalized;
-            
-            float speed = myRigidbody.velocity.magnitude;
-            myRigidbody.velocity = direction * speed;
+            Vector3 barPos = collision.transform.position;      //Barの位置を取得
+            Vector3 ballPos = myTransform.position;             //ボールの位置を取得
+            Vector3 direction = (ballPos - barPos).normalized;  //Barから見たボールの方向を計算
+
+            float speed = myRigidbody.velocity.magnitude;       //現在の速さを取得
+            myRigidbody.velocity = direction * speed;           //速度を変更
         }
+
         if(collision.gameObject.tag == "Wall")
         {
             audioSource.PlayOneShot(bound_sound);
