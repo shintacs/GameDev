bde841c488f2ba4e9a0c6557a77697bc2874acb6
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-06-18 02:12:21 +0900

    Update Game.h

--- CppGameProg/Chap1/HeaderFiles/Game.h
+++ CppGameProg/Chap1/HeaderFiles/Game.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "SDL.h"
+#include <vector>
 
 struct Vector2
 {
 	float x;
 	float y;
+};
+
+struct Ball
+{
+	Vector2 pos;
+	Vector2 vel;
 };
 
 class Game
@@ -33,15 +40,20 @@ private:
 
 	//パドルの位置
 	Vector2 mPaddlePos;
+	Vector2 mPaddleRPos;
 	//パドルの高さ
 	int paddleH;
 	//パドルを動かす
 	int mPaddleDir;
+	int mPaddleRDir;
 
 	//ボールの位置
 	Vector2 mBallPos;
 	//ボールの速度
 	Vector2 mBallVel;
+	//ボールの構造体
+	std::vector<Ball> balls;
+	Ball ball;
 
 	//デルタタイム
 	Uint32 mTicksCount;
