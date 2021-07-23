fa6e8b4e91ee8b087fa28211b3c9444283ce27fe
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-07-23 10:23:37 +0900

    Create Component.cpp

---
+++ CppGameProg/Chap2/SourceFiles/Component.cpp
@@ -0,0 +1,19 @@
+#include "Component.h"
+#include "Actor.h"
+
+Component::Component(Actor* owner, int updateOrder)
+	:mOwner(owner)
+	, mUpdateOrder(updateOrder)
+{
+	//アクターのvectorコンポーネントを追加
+	mOwner->AddComponent(this);
+}
+
+Component::~Component()
+{
+	mOwner->RemoveComponent(this);
+}
+
+void Component::Update(float deltaTime)
+{
+}
