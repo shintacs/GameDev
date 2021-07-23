2bab782090743ee2c31c82f3a66e3d8804e54e1f
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-07-23 10:22:56 +0900

    Create Actor.cpp

---
+++ CppGameProg/Chap2/SourceFiles/Actor.cpp
@@ -0,0 +1,73 @@
+#include "Actor.h"
+#include "Game.h"
+#include "Component.h"
+#include <algorithm>
+
+Actor::Actor(Game* game)
+	:mState(EActive)
+	, mPosition(Vector2::Zero)
+	, mScale(1.0f)
+	, mRotation(0.0f)
+	, mGame(game)
+{
+	mGame->AddActor(this);
+}
+
+Actor::~Actor()
+{
+	mGame->RemoveActor(this);
+	//コンポーネントを削除する必要がある（コンポーネントのデストラクタとは違うループ仕様である必要があるため）
+	while (!mComponents.empty())
+	{
+		delete mComponents.back();
+	}
+}
+
+void Actor::Update(float deltaTime)
+{
+	if (mState == EActive)
+	{
+		UpdateComponents(deltaTime);
+		UpdateActor(deltaTime);
+	}
+}
+
+void Actor::UpdateComponents(float deltaTime)
+{
+	for (auto comp : mComponents)
+	{
+		comp->Update(deltaTime);
+	}
+}
+
+void Actor::UpdateActor(float deltaTime)
+{
+}
+
+void Actor::AddComponent(Component* component)
+{
+	//整列済みのvectorに対する挿入位置を見つける
+	int myOrder = component->GetUpdateOrder();
+	auto iter = mComponents.begin();
+	for (;
+		iter != mComponents.end();
+		++iter)
+	{
+		if (myOrder < (*iter)->GetUpdateOrder())
+		{
+			break;
+		}
+	}
+
+	//イテレータの手前に要素を挿入
+	mComponents.insert(iter, component);
+}
+
+void Actor::RemoveComponent(Component* component)
+{
+	auto iter = std::find(mComponents.begin(), mComponents.end(), component);
+	if (iter != mComponents.end())
+	{
+		mComponents.erase(iter);
+	}
+}
