0250525352834b80d91a3e6705fcc4001e7d0b05
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-08-07 18:51:49 +0900

    Update AnimSpriteComponent.cpp

--- CppGameProg/Chap2/SourceFiles/AnimSpriteComponent.cpp
+++ CppGameProg/Chap2/SourceFiles/AnimSpriteComponent.cpp
@@ -1,10 +1,13 @@
 #include "AnimSpriteComponent.h"
 #include "Math.h"
+#include <iostream>
 
 AnimSpriteComponent::AnimSpriteComponent(Actor* owner, int drawOrder)
 	:SpriteComponent(owner, drawOrder)
 	, mCurrFrame(0.0f)
 	, mAnimFPS(24.0f)
+	, mStart(0.0f)
+	, mEnd(4.0f)
 {
 }
 
@@ -18,12 +21,38 @@ void AnimSpriteComponent::Update(float deltaTime)
 		mCurrFrame += mAnimFPS * deltaTime;
 
 		//必要に応じてカレントフレームを巻き戻す
-		while (mCurrFrame >= mAnimTextures.size())
+		while (mCurrFrame >= mEnd)
 		{
-			mCurrFrame -= mAnimTextures.size();
+			mCurrFrame -= mEnd;
+			mCurrFrame += mStart;
 		}
 
 		//現時点のテクスチャを設定する
 		SetTexture(mAnimTextures[static_cast<int>(mCurrFrame)]);
+	}
+}
+
+void AnimSpriteComponent::ProcessKeyboard(const uint8_t* state)
+{
+	if (state[SDL_SCANCODE_K])
+	{
+		mStart = 5.0f;
+		mEnd = 17.0f;
+	}
+	if (state[SDL_SCANCODE_L])
+	{
+		mStart = 0.0f;
+		mEnd = 4.0f;
+	}
+}
+
+void AnimSpriteComponent::SetAnimTextures(const std::vector<SDL_Texture*>& textures)
+{
+	mAnimTextures = textures;
+	if (mAnimTextures.size() > 0)
+	{
+		//アクティブなテクスチャを最初のフレームにセット
+		mCurrFrame = mStart;
+		SetTexture(mAnimTextures[mCurrFrame]);
 	}
 }
